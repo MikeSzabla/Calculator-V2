bbf380671892a611ec657b45a86b6f3f073aab29
MikeSzabla <ms2667@njit.edu>
2021-11-12 12:50:15 -0500

    adds fixture for calculator_test.py

--- calc/calculator.py
+++ calc/calculator.py
@@ -11,6 +11,11 @@ class Calculator:
     history = []
 
     @staticmethod
+    def get_history():
+        """returns the history list storing past operations"""
+        return Calculator.history
+
+    @staticmethod
     def add_calculation_to_history(calculation):
         """adds a calculation to the end of the history"""
         Calculator.history.append(calculation)
@@ -18,27 +23,27 @@ class Calculator:
     @staticmethod
     def get_result_of_last_calculation():
         """returns the results of the last item in the calculator history"""
-        return Calculator.history[-1].get_result()
+        return Calculator.get_history()[-1].get_result()
 
     @staticmethod
     def get_last_operation():
         """grabs the latest operation from the history"""
-        return Calculator.history[-1]
+        return Calculator.get_history()[-1]
 
     @staticmethod
     def get_result_of_first_calculation():
         """returns the results of the first item in the calculator history"""
-        return Calculator.history[0].get_result()
+        return Calculator.get_history()[0].get_result()
 
     @staticmethod
     def get_first_operation():
         """grabs the first operation from the history"""
-        return Calculator.history[0]
+        return Calculator.get_history()[0]
 
     @staticmethod
     def get_history_length():
         """gets the length of the calculator history"""
-        return len(Calculator.history)
+        return len(Calculator.get_history())
 
     @staticmethod
     def clear_history():
@@ -52,7 +57,7 @@ class Calculator:
 
     @staticmethod
     def add_number(*args):
-        """adds all passed in values"""
+        """adds all passed in args and adds operation to history"""
         Calculator.add_calculation_to_history(Addition.create(*args))
 
     @staticmethod
